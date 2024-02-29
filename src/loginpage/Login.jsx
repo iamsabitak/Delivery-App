47722c7a74b67721e0bd109a3542c1051ff16ba3
sabita <sabitakhadka091@gmail.com>
2024-02-29 12:31:00 +0545

    added default password and email

--- src/loginpage/Login.jsx
+++ src/loginpage/Login.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("default@example.com");
+  const [password, setPassword] = useState("SSaa@@11");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
