95ea09cfff661e60fefb3612f770cf2d875a6833
sabita <sabitakhadka091@gmail.com>
2024-02-29 13:39:32 +0545

    fixed some error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./cartl-list-item/Navbar";
 import Cards from "./cartl-list-item/Cards";
 import ListOrder from "./cartl-list-item/ListOrder";
 import Amount from "./cartl-list-item/Amount";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/");
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       <Navbar />
